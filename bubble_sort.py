45328bc864dd6055a21990a5ba0c9c0942f8c05e
Octavio Serrano <octavioserrano@Octavio-Serranos-MacBook-Pro-10.local>
2021-11-19 15:01:56 -0800

    working bubble function

---
+++ bubble_sort.py
@@ -0,0 +1,14 @@
+def bubble(list_a ):
+    indexing_length = len(list_a) - 1
+    sorted = False
+    
+    while not sorted:
+        sorted = True
+        
+        for i in range(0 , indexing_length):
+            if list_a[i] > list_a[i + 1]:
+                sorted = False
+                list_a[i], list_a[i + 1] = list_a[i + 1], list_a[i]
+    return list_a
+    
+print(bubble([4,9,0,1,-1,0,3,6,764576]))

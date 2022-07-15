f16a89f20de3b4f90e2eafba6f85bc5fe41067ce
serrayon <49975993+serrayon@users.noreply.github.com>
2022-07-15 15:30:29 -0700

    Update insertion_sort.py
    added def main() function

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,12 +1,17 @@
-def insertion(list_a):
-    indexing_length = range(1, len(list_a))
+def insert(list_a):
+    indexing_length = range(1,len(list_a))
     for i in indexing_length:
-        value_to_sort = list_a[i] 
+        value_to_sort = list_a[i]
         
-        while list_a[i - 1] > value_to_sort and i > 0:
-            list_a[i], list_a[i -1] = list_a[i -1],list_a[i]
-            i = i - 1
+        while list_a[i-1] > value_to_sort and i > 0:
+            list_a[i], list_a[i-1] = list_a[i-1], list_a[i]
+            i = i -1 
     return list_a
 
-print(insertion([3,6,6534,7,83,54325,0]))
+def main():
     
+    bin =[423,-2,0,33,-2,66,90,66,-.5]
+    #print (insert([4,34,1,0,9,0,9,1,2,7,0 -3, -3, .5, 432154]))
+    print (insert(bin))
+if __name__=='__main__':
+    main()

a668fd0eeca06d36661ae5f5742adde51ffe75b5
serrayon <49975993+serrayon@users.noreply.github.com>
2022-07-20 22:52:01 -0700

    index, sort
    added global variable for index, and sort method

--- linear.py
+++ linear.py
@@ -14,3 +14,26 @@ def main():
     print (bin)
 if __name__ == '__main__':
     main()
+
+    
+    
+def linear(input_list, target):
+    
+    input_list.sort()
+    global index
+    index = input_list.index(target)
+    
+    for each in input_list:
+        if each == target:
+            return True
+    return linear
+def main():
+    bin = [3,2,6,73,4,65,7,678,3]
+    target_1 = 2
+    print(linear(bin, target_1)) 
+    print('The index of', target_1, 'is',index )
+    print('The ordered bin is\n',bin)
+    
+if __name__ == '__main__':
+    main()
+    

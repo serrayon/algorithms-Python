e4b7db861557eb169bbd372c373a4779c32fb7cc
serrayon <49975993+serrayon@users.noreply.github.com>
2022-11-03 19:59:04 -0700

    stable/ flaw corected
    added a second version that does not have the min_value variable and it correctly sorted the set with multiple duplicate negative numbers

--- selection_sort.py
+++ selection_sort.py
@@ -1,3 +1,10 @@
+'''
+not stable, this version will incorrectly place -2 in the wrong spot. The second version without the min_value variable works though. Quick sort, heap sort and selection not stable'''
+#bubble,merge, insertion, and count are unstable
+
+
+
+
 def selection(list_a):
     indexing_length = range(0, len(list_a)-1)
     for i in indexing_length:
@@ -7,6 +14,16 @@ def selection(list_a):
             if list_a[j] < list_a[min_value]:
                 min_value = j
             if j != i:
-                list_a[min_value], list_a[i] = list_a[i], list_a[min_value]
+                list_a[i], list_a[min_value] = list_a[min_value], list_a[i]
     return list_a
-print(selection([3,5,25,62,6,6]))
+bin1 = [9,8,7,6,5,4,3,2,0,-9,0,-9,65,-2,-2,0]
+print(selection(bin))
+
+def selection(arr):
+    for i in range(len(arr)-1):
+        for j in range(i+1,len(arr)):
+            if arr[j] < arr[i]:
+                arr[i], arr[j] = arr[j], arr[i]
+    return arr
+bin = [9,8,7,6,5,4,3,2,0,-9,0,-9,65,-2,-2,0]
+print(selection(bin))

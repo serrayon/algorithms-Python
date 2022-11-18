9556895139e2fa9cbbc66e2dc5f5032dda3d0a82
serrayon <49975993+serrayon@users.noreply.github.com>
2022-11-18 14:00:30 -0800

    time
    time module

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,4 +1,7 @@
+import time
+
 def bubble(list_a ):
+    
     indexing_length = len(list_a) - 1
     sorted = False
     
@@ -10,5 +13,11 @@ def bubble(list_a ):
                 sorted = False
                 list_a[i], list_a[i + 1] = list_a[i + 1], list_a[i]
     return list_a
-    
-print(bubble([4,9,0,1,-1,0,3,6,764576]))
+def main():
+    st = time.time()   
+    print(bubble([4,9,0,1,-1,0,3,6,764576]))
+    et = time.time()
+    elapsedTime = et - st
+    print(f'The elapsed time is: {elapsedTime} seconds')
+if __name__=='__main__':
+    main()

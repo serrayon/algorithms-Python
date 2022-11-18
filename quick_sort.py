a1446e5f71ebac4ed9f49d2dddab59d38aa69f2f
serrayon <49975993+serrayon@users.noreply.github.com>
2022-11-18 13:51:01 -0800

    time module
    added time module

--- quick_sort.py
+++ quick_sort.py
@@ -1,3 +1,5 @@
+import time
+
 def quick(sequence):
     length = len(sequence)
     if length <= 1:
@@ -17,11 +19,12 @@ def quick(sequence):
     return quick(lower_list) + [pivot] + quick(greater_list)
 
 def main():
-    
+    st = time.time()
     bin = [45,-3,0,64,8765876,23,23,.2]
     #print (quick([3,6,54,12,78,9,0,0,2,1,2,-9,4321542,.5,-5,-5,-.5,-10,-10]))
     print (quick(bin))
-    
+    et = time.time()
+    elapsedTime = et - st 
+    print(f'The elapsed time is: {elapsedTime} seconds')
 if __name__ == '__main__':
     main()
-   

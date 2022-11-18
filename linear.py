70159cf331b7917f89660fd891f950fde183f8a9
serrayon <49975993+serrayon@users.noreply.github.com>
2022-11-18 14:18:56 -0800

    time
    time module and corrected some errors on the second  algo

--- linear.py
+++ linear.py
@@ -1,3 +1,4 @@
+import time
 def linear_search(input_list, target_value):
     input_list.sort()
     for each in input_list:
@@ -7,31 +8,32 @@ def linear_search(input_list, target_value):
     
     
 def main():
-    
+    st = time.time()
     bin = [3,2,6,73,4,65,7,678,3]
     target_1 = 2
     print (linear_search(bin,target_1))
     print (bin)
+    et = time.time()
+    elapsedTime = et - st 
+    print(f'The elapsed time is: {elapsedTime} seconds')
 if __name__ == '__main__':
     main()
 
-    
-    
+    #deleted the index was not working, figure it out later
 def linear(input_list, target):
     input_list.sort()
     
     for each in input_list:
-        index = input_list.index(target)
+        
         if each == target:
-            return True, target,index, inputList
-    return linear
+            
+            return True, target, input_list
+    return False
 def main():
     bin = [3,2,6,73,4,65,7,678,3]
     target_1 = 2
     print(linear(bin, target_1)) 
-    print('The index of', target_1, 'is',index )
     print('The ordered bin is\n',bin)
     
 if __name__ == '__main__':
     main()
-    

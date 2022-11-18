7004c0b673ae806fba8a4d8f1fe7484ce6214f07
serrayon <49975993+serrayon@users.noreply.github.com>
2022-11-18 13:54:29 -0800

    time
    time module

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,3 +1,4 @@
+import time 
 def insert(list_a):
     indexing_length = range(1,len(list_a))
     for i in indexing_length:
@@ -9,9 +10,12 @@ def insert(list_a):
     return list_a
 
 def main():
-    
+    st = time.time()
     bin =[423,-2,0,33,-2,66,90,66,-.5]
     #print (insert([4,34,1,0,9,0,9,1,2,7,0 -3, -3, .5, 432154]))
     print (insert(bin))
+    et = time.time()
+    elapsedTime = et - st 
+    print(f'Elapsed time is: {elapsedTime} seconds')
 if __name__=='__main__':
     main()

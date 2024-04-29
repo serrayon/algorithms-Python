8b10d877f41a797763addac89d9e988062101cb8
serrayon <49975993+serrayon@users.noreply.github.com>
2024-04-28 19:29:43 -0700

    Create bfs_with_deque.py
    If working with larger graphs this will be best

---
+++ bfs_with_deque.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+    
+    while queue:
+        vertex = queue.popleft()
+        if vertex not in visited:
+            print(vertex, end=' ')
+            visited.add(vertex)
+            queue.extend(graph[vertex] - visited)
+
+# Example graph represented as an adjacency list
+graph = {
+    'A': {'B', 'C'},
+    'B': {'A', 'D', 'E'},
+    'C': {'A', 'F'},
+    'D': {'B'},
+    'E': {'B', 'F'},
+    'F': {'C', 'E'}
+}
+
+# Starting vertex for BFS
+start_vertex = 'A'
+
+print("BFS Traversal:")
+bfs(graph, start_vertex)
